b80ce4d6ce0b6252564e69efb87adc21b1f8ac2f
isamaues <42849591+isamaues@users.noreply.github.com>
2021-12-05 13:19:46 -0300

    jogo com novos eventos

--- Jogo/main.py
+++ Jogo/main.py
@@ -4,7 +4,7 @@ kivy.require('1.1.1')
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import (
-    NumericProperty, ReferenceListProperty, ObjectProperty, BooleanProperty
+    NumericProperty, ReferenceListProperty, ObjectProperty, BooleanProperty, StringProperty
 )
 from kivy.vector import Vector
 from kivy.clock import Clock
@@ -18,6 +18,7 @@ class PongPaddle(Widget):
 
     def bounce_ball(self, ball): #toda vez que a função é chamada, realizar uma vibração curta (ping, pong..)
         if self.collide_widget(ball) and self.can_bounce:
+            ## self.bt.send(code='4')
             vx, vy = ball.velocity
             offset = (ball.center_y - self.center_y) / (self.height / 2)
             bounced = Vector(-1 * vx, vy)
@@ -42,13 +43,16 @@ class PongGame(Widget):
     ball = ObjectProperty(None)
     player1 = ObjectProperty(None)
     player2 = ObjectProperty(None)
+    pause_game = BooleanProperty(False)
+    endgame_message = StringProperty('')
+
     def __init__(self):
         super(PongGame, self).__init__()
         self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
         self._keyboard.bind(on_key_down = self._on_keyboard_down)
         self._keyboard.bind(on_key_up = self._on_keyboard_up)
         self.pressed_keys = set()
-        self.bt = Bluetooth()
+        ## self.bt = Bluetooth()
 
     def _keyboard_closed (self):
         self._keyboard.unbind(on_key_down=self._on_keyboard_down)
@@ -60,13 +64,7 @@ class PongGame(Widget):
     def _on_keyboard_up(self, keyboard, keycode):
         self.pressed_keys.remove(keycode[1])
 
-    def serve_ball(self, vel=(4, 0)): #toda vez que a função é chamada, realizar uma vibração longa (vooosh)- dar uma pausa antes do efeito para que tenha a vibração correspondente ao jogador que venceu
-        self.ball.center = self.center
-        self.ball.velocity = vel
-#1: ..-
-#2: _..
-# ou fazer o jogo só para um jogador já que estou fazendo só para uma pulseira (perguntar para a prof)
-    def player_position_update(self):
+    def _player_position_update(self):
         if 'w' in self.pressed_keys:
             self.player1.center_y += 15
         if 's' in self.pressed_keys:
@@ -76,8 +74,40 @@ class PongGame(Widget):
         if 'down' in self.pressed_keys:
             self.player2.center_y -= 15
 
+    def serve_ball(self, vel=(4, 0)): #toda vez que a função é chamada, realizar uma vibração longa (vooosh)- dar uma pausa antes do efeito para que tenha a vibração correspondente ao jogador que venceu
+        self.ball.center = self.center
+        self.ball.velocity = vel
+        ## self.bt.send(code='3')
+        
+#1: ..-
+#2: _..
+# ou fazer o jogo só para um jogador já que estou fazendo só para uma pulseira (perguntar para a prof)
+#     
+    def endgame(self, max_score = 5):
+        flag = False
+        if self.player1.score >= max_score:
+            self.endgame_message = "[color=3333ff]Jogador 1 venceu[/color]"
+            flag = True
+        elif self.player2.score >= max_score:
+            self.endgame_message = "[color=#ff820d]Jogador 2 venceu[/color]"
+            flag = True
+    
+        if flag:
+            self.pause_game = True
+            Clock.schedule_once(self.start_game, 5)
+            self.player1.score = 0
+            self.player2.score = 0
+            ## self.bt.send(code='5')
+
+    def start_game(self, *args):
+        self.endgame_message = ''
+        self.pause_game = False
+
     def update(self, dt):
-        self.player_position_update()
+        if self.pause_game:
+            return
+
+        self._player_position_update()
         self.ball.move()
 
         # bounce ball off paddles
@@ -92,17 +122,13 @@ class PongGame(Widget):
         if self.ball.x < self.x:
             self.player2.score += 1
             self.serve_ball(vel=(4, 0))
-            self.bt.send(code='A')
+            ## self.bt.send(code='2')
         if self.ball.right > self.width:
             self.player1.score += 1
             self.serve_ball(vel=(-4, 0))
-            self.bt.send(code='B')
+            ## self.bt.send(code='1')
 
-    def on_touch_move(self, touch):
-        if touch.x < self.width / 3:
-            self.player1.center_y = touch.y
-        if touch.x > self.width - self.width / 3:
-            self.player2.center_y = touch.y
+        self.endgame(5)
 
 
 class PongApp(App):
@@ -111,6 +137,17 @@ class PongApp(App):
         game.serve_ball()
         Clock.schedule_interval(game.update, 1.0 / 60.0)
         return game
+
+# Eventos
+# 1 - Jogador 1 faz um ponto
+# 2 - Jogador 2 faz um ponto
+# 3 - Bola servida
+# 4 - Bola quica
+# 5 - Fim de jogo
+
+# Novas funcionalidades
+# 1 - Fim de jogo com 5 pontos
+# 2 - Delay de "animação" depois que um jogador faz um ponto ate a bola ser servida
 
 
 if __name__ == '__main__':
